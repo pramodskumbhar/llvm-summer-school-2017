4dade84befc3ea0c3a5de56e3edcc392a9f97f5b
Pramod Kumbhar <pramod.kumbhar@epfl.ch>
2017-06-17 17:24:34 +0200

    Updated code from last day lab

--- loop-vectorizer-skeleton/README.md
+++ loop-vectorizer-skeleton/README.md
@@ -1,16 +1,116 @@
-# llvm-pass-skeleton
+## LLVM Pass Skeleton
 
-A completely useless LLVM pass.
+Simple example of LLVM pass from summer school. This was compiled/run with clang/llvm trunk installation.
 
-Build:
+### Build
 
-    $ cd llvm-pass-skeleton
-    $ mkdir build
-    $ cd build
-    $ cmake ..
-    $ make
-    $ cd ..
+Once you have Clang/LLVM trunk installed, you can build this pass as:
 
-Run:
+```bash
+export LLVM_INSTALL_PATH=$HOME/workarena/repos/ext/paris-clang-llvm-2017/pramodskumbhar/Scripts/install/debug/trunk:$PATH
+export PATH=$LLVM_INSTALL_PATH/bin:$PATH
 
-    $ clang -Xclang -load -Xclang build/skeleton/libSkeletonPass.* something.c
+cd loop-vectorizer-skeleton
+mkdir -p build && cd build
+cmake .. -DCMAKE_C_COMPILER=`which clang` -DCMAKE_CXX_COMPILER=`which clang++` -DCMAKE_BUILD_TYPE=Debug -DLLVM_CONFIG=$LLVM_INSTALL_PATH/bin/llvm-config -DCMAKE_CXX_FLAGS="-std=c++11"
+make VERBOSE=1
+```
+### Loop Canonicalization
+
+First generate LLVM IR file from test code. Note that we have to use higher optimization flags (e.g. `-O3`) in order to generate metadata information which is required during optimization passes. If we don't do this then optimization passes doesn't run.
+
+```bash
+ clang  -emit-llvm -O3 -mllvm -disable-llvm-optzns -S -c ../test/vector-addition.cpp -o vector-addition
+```
+
+This will produce the loop which will look like:
+
+```bash
+$ cat vector-addition
+
+define void @_Z15vector_additionPfS_(float* %A, float* %B) #0 {
+entry:
+  ...
+  br label %for.cond
+
+for.cond:                                         ; preds = %for.inc, %entry
+  %1 = load i64, i64* %i, align 8, !tbaa !7
+  %cmp = icmp slt i64 %1, 1024
+  br i1 %cmp, label %for.body, label %for.cond.cleanup
+
+for.cond.cleanup:                                 ; preds = %for.cond
+  %2 = bitcast i64* %i to i8*
+  call void @llvm.lifetime.end.p0i8(i64 8, i8* %2) #2
+  br label %for.end
+
+for.body:                                         ; preds = %for.cond
+  ...
+  store float %add, float* %arrayidx1, align 4, !tbaa !9
+  br label %for.inc
+
+for.inc:                                          ; preds = %for.body
+  %9 = load i64, i64* %i, align 8, !tbaa !7
+  %inc = add nsw i64 %9, 1
+  store i64 %inc, i64* %i, align 8, !tbaa !7
+  br label %for.cond
+```
+
+The above loop is not in canonical form. (see unconditional branch from `entry` block to `for.cond`). We can apply some loop transformation passes to turn it into canonical form. This can be achieved by:
+
+```bash
+$ opt -load -mem2reg -instnamer -loop-rotate  -S  vector-addition
+
+entry:
+  ...
+  %cmp11 = icmp slt i64 %tmp110, 1024
+  br i1 %cmp11, label %for.body.lr.ph, label %for.cond.cleanup
+
+for.body.lr.ph:                                   ; preds = %entry
+  br label %for.body
+
+for.cond.for.cond.cleanup_crit_edge:              ; preds = %for.inc
+  br label %for.cond.cleanup
+
+for.cond.cleanup:                                 ; preds = %for.cond.for.cond.cleanup_crit_edge, %entry
+  %tmp2 = bitcast i64* %i to i8*
+  call void @llvm.lifetime.end.p0i8(i64 8, i8* %tmp2) #2
+  br label %for.end
+
+for.body:
+  ...                                             ; preds = %for.body.lr.ph, %for.inc
+  br label %for.inc
+
+for.inc:
+  ...                                             ; preds = %for.body
+  br i1 %cmp, label %for.body, label %for.cond.for.cond.cleanup_crit_edge
+```
+
+Notice the unconditional branch is now replaced by conditional.
+
+### Running A Pass
+
+Now we can run the pass using `opt` tool. Build the code as :
+
+```bash
+cd build
+cmake .. -DCMAKE_C_COMPILER=`which clang` -DCMAKE_CXX_COMPILER=`which clang++` -DCMAKE_BUILD_TYPE=Debug -DLLVM_CONFIG=$LLVM_INSTALL_PATH/bin/llvm-config -DCMAKE_CXX_FLAGS="-std=c++11"
+make VERBOSE=1
+```
+
+And run pass as:
+
+```bash
+opt -load simple-vectorizer/libSimpleVectorizerPass.so -mem2reg -instnamer -loop-rotate -simple-vectorizer vector-addition
+```
+
+If you want to print / dump IR then use `-S` option:
+
+```bash
+opt -load simple-vectorizer/libSimpleVectorizerPass.so -mem2reg -instnamer -loop-rotate  -simple-vectorizer  -S  vector-addition
+```
+
+When you want to check/debug the passes being run, use `debug-pass` option as:
+
+```bash
+opt -load simple-vectorizer/libSimpleVectorizerPass.so -mem2reg -instnamer -loop-rotate  -simple-vectorizer  -S  vector-addition -debug-pass=Executions
+```
--- loop-vectorizer-skeleton/simple-vectorizer/SimpleVectorizer.cpp
+++ loop-vectorizer-skeleton/simple-vectorizer/SimpleVectorizer.cpp
@@ -8,159 +8,242 @@
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/Transforms/IPO/PassManagerBuilder.h"
 #include "llvm/Transforms/Utils/BasicBlockUtils.h"
+#include <llvm/Analysis/ScalarEvolutionExpressions.h>
+#include <iostream>
+#include <algorithm>
 
 #include <vector>
 
 using namespace llvm;
 
 namespace {
-struct SimpleVectorizer : public FunctionPass {
-  static char ID;
-  static const int VectorWidth = 4;
+    struct SimpleVectorizer : public FunctionPass {
+        static char ID;
+        static const int VectorWidth = 4;
 
-  LoopInfo *LI;
-  ScalarEvolution *SE;
+        LoopInfo *LI;
+        ScalarEvolution *SE;
 
-  // The set of loops we want to vectorize.
-  std::vector<Loop *> LoopsToVectorize;
+        // The set of loops we want to vectorize.
+        std::vector<Loop *> LoopsToVectorize;
 
-  // A map that contains for each original value the corresponding new
-  // scalarized value for a given scalar iteration.
-  std::map<std::pair<int, Value *>, Value *> ValueMap;
+        // A map that contains for each original value the corresponding new
+        // scalarized value for a given scalar iteration.
+        std::map<std::pair<int, Value *>, Value *> ValueMap;
 
-  // A map that contains for each original value the corresponding new
-  // vector value.
-  std::map<Value *, Value *> VectorMap;
+        // A map that contains for each original value the corresponding new
+        // vector value.
+        std::map<Value *, Value *> VectorMap;
 
-  SimpleVectorizer() : FunctionPass(ID) {}
+        SimpleVectorizer() : FunctionPass(ID) {}
 
-  // Return true if the loop is innermost.
-  bool isInnermostLoop(Loop *L);
+        // Return true if the loop is innermost.
+        bool isInnermostLoop(Loop *L);
 
-  // Return true if we are capable of vectorizing the loop.
-  bool isValidLoop(Loop *L);
+        // Return true if we are capable of vectorizing the loop.
+        bool isValidLoop(Loop *L);
 
-  // Collect loops that can be vectorized.
-  void collectLoops(Function &F);
+        // Collect loops that can be vectorized.
+        void collectLoops(Function &F);
 
-  // Print loops that should be vectorized.
-  void printLoops(Function &F);
+        // Print loops that should be vectorized.
+        void printLoops(Function &F);
 
-  // Transform the loops that should be vectorized.
-  void transformLoops();
+        // Transform the loops that should be vectorized.
+        void transformLoops();
 
-  // Transform the Loop *L
-  void transformLoop(Loop *L);
+        // Transform the Loop *L
+        void transformLoop(Loop *L);
 
-  // Return the single basic block that represents the body of @p L.
-  BasicBlock *getSingleBodyBlock(Loop *L);
+        // Return the single basic block that represents the body of @p L.
+        BasicBlock *getSingleBodyBlock(Loop *L);
 
-  // Check if the loop structure of @p L is simple (canonical).
-  bool isSimpleLoopStructure(Loop *L);
+        // Check if the loop structure of @p L is simple (canonical).
+        bool isSimpleLoopStructure(Loop *L);
 
-  // Get the new value for a given original value in the scalar lane @p Lane.
-  Value *getNewValue(int Lane, Value *V);
+        // Get the new value for a given original value in the scalar lane @p Lane.
+        Value *getNewValue(int Lane, Value *V);
 
-  // Get the PHI node which serves as induction variable for @p L.
-  Value *getIndVar(Loop *L);
+        // Get the PHI node which serves as induction variable for @p L.
+        Value *getIndVar(Loop *L);
 
-  // Check if the loop body of @p L only contains instructions we can vectorize.
-  bool isValidBody(Loop *L);
+        // Check if the loop body of @p L only contains instructions we can vectorize.
+        bool isValidBody(Loop *L);
 
-  // Get a scalar evolution expression that represents the memory access
-  // location of @p Inst.
-  const SCEV *getPointerSCEV(Instruction *Inst);
+        // Get a scalar evolution expression that represents the memory access
+        // location of @p Inst.
+        const SCEV *getPointerSCEV(Instruction *Inst);
 
-  // Get the size of the elements (in bytes) loaded or stored by Inst.
-  size_t getElementSize(Instruction *Inst);
+        // Get the size of the elements (in bytes) loaded or stored by Inst.
+        size_t getElementSize(Instruction *Inst);
 
-  // Check if an instruction @p Inst has stride one access in loop @p L.
-  bool isStrideOneAccess(Instruction *Inst, Loop *L);
+        // Check if an instruction @p Inst has stride one access in loop @p L.
+        bool isStrideOneAccess(Instruction *Inst, Loop *L);
 
-  virtual bool runOnFunction(Function &F) {
-    LoopsToVectorize.clear();
-    ValueMap.clear();
+        virtual bool runOnFunction(Function &F) {
+            LoopsToVectorize.clear();
+            ValueMap.clear();
 
-    LI = &getAnalysis<LoopInfoWrapperPass>().getLoopInfo();
-    SE = &getAnalysis<ScalarEvolutionWrapperPass>().getSE();
+            LI = &getAnalysis<LoopInfoWrapperPass>().getLoopInfo();
+            SE = &getAnalysis<ScalarEvolutionWrapperPass>().getSE();
 
-    errs() << "I saw a function called " << F.getName() << "!\n";
+            errs() << "==> See function with name " << F.getName() << "!\n";
 
-    collectLoops(F);
-    printLoops(F);
-    transformLoops();
+            collectLoops(F);
+            printLoops(F);
+            transformLoops();
 
-    return false;
-  }
+            return false;
+        }
 
-  void getAnalysisUsage(AnalysisUsage &AU) const {
-    AU.addRequired<LoopInfoWrapperPass>();
-    AU.addRequired<ScalarEvolutionWrapperPass>();
-  }
-};
+        void getAnalysisUsage(AnalysisUsage &AU) const {
+            AU.addRequired<LoopInfoWrapperPass>();
+            AU.addRequired<ScalarEvolutionWrapperPass>();
+        }
+    };
 
-bool SimpleVectorizer::isInnermostLoop(Loop *L) {
-}
+    bool SimpleVectorizer::isInnermostLoop(Loop *L) {
+        auto innerLoops = L->getSubLoops();
+        return  innerLoops.size() == 0;
+    }
 
-bool SimpleVectorizer::isValidLoop(Loop *L) {
-}
+    void SimpleVectorizer::collectLoops(Function &F) {
+        for (auto &bb : F) {
+            auto loop = LI->getLoopFor(&bb);
+            if(loop != nullptr) {
+                // multiple basic blocks are part of same loop and hence check if
+                // loop is already added to the vector
+                bool exist = std::find(LoopsToVectorize.begin(), LoopsToVectorize.end(), loop) != LoopsToVectorize.end();
+                if (!exist) {
+                    // need to vectorize only innermost loop
+                    if(isInnermostLoop(loop)) {
+                        LoopsToVectorize.push_back(loop);
+                    }
+                }
+            }
+        }
+    }
 
-const SCEV *SimpleVectorizer::getPointerSCEV(Instruction *Inst) {
-}
+    void SimpleVectorizer::printLoops(Function &F) {
+        errs() << "==> Collected loops with iteration count (SCEV) => \n";
+        for(auto loop: LoopsToVectorize) {
+            loop->dump();
+            SE->getBackedgeTakenCount(loop)->dump();
+        }
+    }
 
-size_t SimpleVectorizer::getElementSize(Instruction *Inst) {
-}
+    void SimpleVectorizer::transformLoops() {
 
-bool SimpleVectorizer::isStrideOneAccess(Instruction *Inst, Loop *L) {
-}
+        for(auto loop: LoopsToVectorize) {
 
-void SimpleVectorizer::collectLoops(Function &F) {
-}
+            auto body = getSingleBodyBlock(loop);
 
-void SimpleVectorizer::printLoops(Function &F) {
-}
+            if(body) {
 
-void SimpleVectorizer::transformLoops() {
-}
+                errs() << "==> Body of the loop is : \n";
+                body->dump();
 
-BasicBlock *SimpleVectorizer::getSingleBodyBlock(Loop *L) {
-}
+                for (auto &I : *body) {
+                    if (LoadInst *LI = dyn_cast<LoadInst>(&I)) {
+                        errs() << "==> Load Instruction in the loop : \n";
+                        LI->dump();
 
-bool SimpleVectorizer::isValidBody(Loop *L) {
-}
+                        // we have load from memory, ged directly the first operand
+                        auto *operand = LI->getOperand(0);
+                        auto *scev = (SCEVAddRecExpr *) SE->getSCEV(operand);
 
-bool SimpleVectorizer::isSimpleLoopStructure(Loop *L) {
-}
 
-Value *SimpleVectorizer::getNewValue(int Lane, Value *V) {
-}
+                        errs() << "==> SCEV for load instruction is : \n";
+                        scev->dump();
 
-Value *SimpleVectorizer::getIndVar(Loop *L) {
-}
+                        if (scev->isAffine()) {
+                            std::cout << "==> SCEV is Affine expression !\n";
+                        } else {
+                            std::cout << "==> SCEV is NOT Affine expression !\n";
+                        }
+                    }
+                }
+            }
+        }
+    }
 
-void SimpleVectorizer::transformLoop(Loop *L) {
-}
+    BasicBlock *SimpleVectorizer::getSingleBodyBlock(Loop *L) {
+        auto blocks = L->getBlocks();
+        auto exitBlock = L->getExitBlock();
+
+        // assuming simple case where we have only two blocks i.e. body and exit block
+        for(auto block: blocks) {
+            if(block != exitBlock)
+                return block;
+        }
+
+        std::cout << "\n Error : Can't find body block! \n";
+        return nullptr;
+    }
+
+    bool SimpleVectorizer::isValidBody(Loop *L) {
+        // todo
+        return true;
+    }
+
+    bool SimpleVectorizer::isSimpleLoopStructure(Loop *L) {
+        // todo
+        return true;
+    }
+
+    Value *SimpleVectorizer::getNewValue(int Lane, Value *V) {
+        // todo
+        return nullptr;
+    }
+
+    Value *SimpleVectorizer::getIndVar(Loop *L) {
+        // todo
+        return nullptr;
+    }
+
+    void SimpleVectorizer::transformLoop(Loop *L) {
+        // todo
+    }
+
+    bool SimpleVectorizer::isValidLoop(Loop *L) {
+        // todo
+        return true;
+    }
+
+    const SCEV *SimpleVectorizer::getPointerSCEV(Instruction *Inst) {
+        // todo
+        return nullptr;
+    }
+
+    size_t SimpleVectorizer::getElementSize(Instruction *Inst) {
+        // todo
+        return 0;
+    }
+
+    bool SimpleVectorizer::isStrideOneAccess(Instruction *Inst, Loop *L) {
+        // todo
+        return true;
+    }
+
 } // namespace
 
 char SimpleVectorizer::ID = 0;
 
 namespace llvm {
-void initializeSimpleVectorizerPass(llvm::PassRegistry &);
+    void initializeSimpleVectorizerPass(llvm::PassRegistry &);
 }
 
 class StaticInitializer {
 public:
-  StaticInitializer() {
-    llvm::PassRegistry &Registry = *llvm::PassRegistry::getPassRegistry();
-    llvm::initializeSimpleVectorizerPass(Registry);
-  }
+    StaticInitializer() {
+        llvm::PassRegistry &Registry = *llvm::PassRegistry::getPassRegistry();
+        llvm::initializeSimpleVectorizerPass(Registry);
+    }
 };
 
 static StaticInitializer InitializeEverything;
 
-INITIALIZE_PASS_BEGIN(SimpleVectorizer, "simple-vectorizer",
-                      "Simple Vectorizer", false, false);
+INITIALIZE_PASS_BEGIN(SimpleVectorizer, "simple-vectorizer", "Simple Vectorizer", false, false);
 INITIALIZE_PASS_DEPENDENCY(LoopInfoWrapperPass);
 INITIALIZE_PASS_DEPENDENCY(ScalarEvolutionWrapperPass);
-INITIALIZE_PASS_END(SimpleVectorizer, "simple-vectorizer", "Simple Vectorizer",
-                    false, false)
+INITIALIZE_PASS_END(SimpleVectorizer, "simple-vectorizer", "Simple Vectorizer", false, false)
